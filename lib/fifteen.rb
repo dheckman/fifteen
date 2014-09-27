c802dbca6c598888716c41bb2f697f729f52162c
Danielle <danielleheckman5@gmail.com>
2014-09-27 16:34:28 -0400

    updated game

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -14,20 +14,13 @@ class Player
       return 0
     end
   end
-  # def check_hand_total
-  #   if @hand != nil
-  #    return @hand.inject(:+).to_i
-  #   else
-  #     return 0
-  #   end
-  # end
 end
 class Fifteen
   attr_accessor :player, :numbers
   def initialize
     @player = Player.new
     @computer = Player.new
-    @numbers = (2..9).to_a
+    @numbers = (1..9).to_a
     @WIN = 15
   end
   def get_name
@@ -46,28 +39,65 @@ class Fifteen
       else
         puts "YOU LOSE. Computer goes first."
         computer_turn
+        run
       end
   end
+  def sum(array)
+    array.inject{|sum,x| sum + x}
+  end
   def pick_num
-    puts "Pick a number between 1 and 9"
-    @num = gets.chomp.to_i
-    @player.hand << @num
-    @numbers.delete(@num)
+      puts "Pick a number between 1 and 9"
+      @num = gets.chomp.to_i
+      @player.hand << @num
+      @numbers.delete(@num)
     puts "You have the following numbers: #{@player.hand}. The remaining numbers are #{@numbers}."
-
-    # computer_turn
   end
   def computer_turn
     @computer_choice = @numbers.sample
     @computer.hand << @computer_choice
     @numbers.delete(@computer_choice)
     puts "The computer chose a(n) #{@computer_choice}. Computer's numbers are: #{@computer.hand}. The remaining numbers are #{@numbers}"
-    run
+
   end
   def run
-    until (@numbers.empty?) || ((@player.hand.include?(@WIN)) || (@computer.hand.include?(@WIN))) do
-      pick_num
-      computer_turn
+    pick_num
+    computer_turn
+    pick_num
+    computer_turn
+    computer_win?
+    pick_num
+    player_win?
+    computer_turn
+    computer_win?
+    pick_num
+    player_win?
+    computer_turn
+    computer_win?
+    if @numbers.empty?
+      puts "Game over, out of numbers! \n Play again?"
+      answer = gets.chomp.downcase
+      if answer == "yes"
+        start
+      else
+        puts "BYEEEEEE."
+      end
+    end
+    pick_num
+  end
+  def computer_win?
+    c = @computer.hand.combination(3).to_a
+    if c.any? { |x| sum(x) == 15 }
+      puts "Game over, computer wins!"
+      exit
+    else
+    end
+  end
+  def player_win?
+    p = @player.hand.combination(3).to_a
+    if p.any? { |x| sum(x) == 15 }
+      puts "Game over, player wins!"
+      exit
+    else
     end
   end
 end
