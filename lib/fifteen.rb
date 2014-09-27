129fc697414b91b8dfb674daab38e54bcff592af
Danielle <danielleheckman5@gmail.com>
2014-09-27 14:25:21 -0400

    updated game

--- lib/fifteen.rb
+++ lib/fifteen.rb
@@ -1,43 +1,75 @@
-require_relative "player"
-
+#require_relative "player"
+class Player
+  attr_accessor :num, :hand
+  def initialize()
+    @hand = []
+  end
+  def pick_num(player)
+    puts "Pick a number between 1 and 9"
+    @num = gets.chomp.to_i
+    @hand << @num
+    if @hand != nil
+     return @hand.inject(:+).to_i
+    else
+      return 0
+    end
+  end
+  # def check_hand_total
+  #   if @hand != nil
+  #    return @hand.inject(:+).to_i
+  #   else
+  #     return 0
+  #   end
+  # end
+end
 class Fifteen
-  attr_accessor :player
+  attr_accessor :player, :numbers
   def initialize
-    puts "Hello! What is your name?"
-    @name = gets.chomp.capitalize
-    puts "Okay #{@name}, let's get started!"
     @player = Player.new
     @computer = Player.new
-    @numbers = [1,2,3,4,5,6,7,8,9]
-    start
+    @numbers = (2..9).to_a
+    @WIN = 15
+  end
+  def get_name
+    puts "Hello! What is your name?"
+    name = gets.chomp.capitalize
+    puts "Okay #{name}, let's get started!"
   end
   def start
-    @heads_or_tails = ["heads","tails"].sample
+    get_name
+    heads_or_tails = ["heads","tails"].sample
     puts "Heads or tails?"
-    @player_answer = gets.chomp.downcase
-      if @player_answer == @heads_or_tails
+    player_answer = gets.chomp.downcase
+      if player_answer == heads_or_tails
         puts "You win!"
-        @player.pick_num
+        run
       else
         puts "YOU LOSE. Computer goes first."
-        @computer.hand << @numbers.sample
-        puts "The computer chose a(n) #{@computer.hand}."
-        puts @computer.hand
+        computer_turn
       end
   end
-  # def check_hands
-  #   puts @hand
-    #   @numbers.delete(@num)
-    # else
-    #   puts "waaaa"
+  def pick_num
+    puts "Pick a number between 1 and 9"
+    @num = gets.chomp.to_i
+    @player.hand << @num
+    @numbers.delete(@num)
+    puts "You have the following numbers: #{@player.hand}. The remaining numbers are #{@numbers}."
 
-  # end
-  def win(hand)
-    #if player or computer has at least three cards that total exactly 15
+    # computer_turn
+  end
+  def computer_turn
+    @computer_choice = @numbers.sample
+    @computer.hand << @computer_choice
+    @numbers.delete(@computer_choice)
+    puts "The computer chose a(n) #{@computer_choice}. Computer's numbers are: #{@computer.hand}. The remaining numbers are #{@numbers}"
+    run
+  end
+  def run
+    until (@numbers.empty?) || ((@player.hand.include?(@WIN)) || (@computer.hand.include?(@WIN))) do
+      pick_num
+      computer_turn
+    end
   end
 end
-
-
-
-
 me = Fifteen.new
+me.start
--- lib/player.rb
+++ lib/player.rb
@@ -1,26 +1,20 @@
 class Player
-  attr_accessor
+  attr_accessor :num, :hand
   def initialize()
     @hand = []
-
   end
-  def hand
-    @hand
-  end
-  def pick_num()
+  def pick_num
     puts "Pick a number between 1 and 9"
     @num = gets.chomp.to_i
     @hand << @num
+    @numbers.delete(@num)
     return @num
   end
-  def check_hand
+  def check_hand_total
     if @hand != nil
-      hand_total
+     return @hand.inject(:+).to_i
     else
       return 0
     end
-  end
-  def hand_total
-    puts @hand.inject(:+)
   end
 end
