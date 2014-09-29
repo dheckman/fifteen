64ed4c9f6286664f8f8394e823b0b3062cb021b2
Danielle <danielleheckman5@gmail.com>
2014-09-28 21:02:44 -0400

    updated test a bit

--- test/player_test.rb
+++ test/player_test.rb
@@ -1,26 +1,28 @@
 require "minitest/autorun"
 require "player"
+require "fifteen"
 
 class PlayerTest < Minitest::Unit::TestCase
   def test_player_initialize
-    assert Player.new
-  end
-
-  def test_new_player_has_empty_hand
-    assert Player.new.hand.empty?
-  end
-
-  def test_player_picks_number
-    Player.new.pick_num == 2
-  end
-
-  def test_check_hand
     p = Player.new
-    orig = p.check_hand(nil)
-    p.pick_num
-    updated = p.check_hand(nil)
-    puts orig
-    puts updated
-    assert updated != orig
+    assert_not_nil p
   end
+
+  # def test_new_player_has_empty_hand
+  #   assert Player.new.hand.empty?
+  # end
+  #
+  # def test_player_picks_number
+  #   Player.new.pick_num == 2
+  # end
+  #
+  # def test_check_hand
+  #   p = Player.new
+  #   orig = p.check_hand(nil)
+  #   p.pick_num
+  #   updated = p.check_hand(nil)
+  #   puts orig
+  #   puts updated
+  #   assert updated != orig
+  # end
 end
